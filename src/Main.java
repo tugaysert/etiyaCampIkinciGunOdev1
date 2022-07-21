87eadffa4b3fb619b43649297cddb350509f6ca8
tugaysert <mustafatugaysert@gmail.com>
2022-07-21 13:20:33 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Course course1 = new Course();
+        Category category1 = new Category();
+        Instructor instructor1 = new Instructor();
+        Student student1 = new Student();
+
+        instructor1.setId(1);
+        instructor1.setFirstName("Engin");
+        instructor1.setLastName("Demirog");
+
+        category1.setId(1);
+        category1.setName("test");
+
+        course1.setId(1);
+        course1.setName("UNITTEST");
+        course1.setCategory(category1);
+        course1.setInstructor(instructor1);
+
+
+        Category category2 = new Category(2, "yazilim");
+        Instructor instructor2 = new Instructor(2, "ENGIN", "KALAYCI");
+        Course course2 = new Course(2, "JAVA-REACT", category2, instructor2);
+
+        System.out.println(course1.getName() + " " + course1.getCategory().getName()
+                + " " + course1.getInstructor().getFirstName()
+                + " " + course1.getInstructor().getLastName());
+
+        System.out.println(course1);
+        System.out.println(course2);
+
+        student1.setId(1);
+        student1.setFirstName("Mahmut");
+        student1.setLastName("Kirazci");
+        student1.setCourseList(Arrays.asList(course1, course2));
+
+        System.out.println(student1);
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private List<Course> courseList = new ArrayList<>();
+
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", courseList=" + courseList +
+                '}';
+    }
+}
